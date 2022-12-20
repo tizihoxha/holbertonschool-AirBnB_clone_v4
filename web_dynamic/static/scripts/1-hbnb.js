ba62ecca3e5fa23cd7ab33dde89559d6cd50e3f7
tizihoxha <tizihoxha@gmail.com>
2022-12-20 03:28:54 -0800

    added

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+$document.ready(function () {
+	const amenityDict = {};
+	$('input[type=checkbox]').change(fucntion () {
+		if ($(this).is(':checked')) {
+			amenityDict[$(this).attr('data-id')] = $(this).attr('data.name');
+		} else {
+			delete amenityDict[$(this).attr('data-id')];
+		}
+		$('.amenities h4').text(Object.values(amenityDict).join(', '));
+	});
+});
