3a2282da857402a042c96728c2715ed72c880419
FatjonMileti <tonimileti@yahoo.com>
2022-12-21 02:37:42 -0800

    ...

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -10,7 +10,7 @@ $('document').ready(function () {
 	const amenityDict = {};
 	$('input[type=checkbox]').change(function () {
 		if ($(this).is(':checked')) {
-			amenityDict[$(this).attr('data-id')] = $(this).attr('data.name');
+			amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
 		} else {
 			delete amenityDict[$(this).attr('data-id')];
 		}
