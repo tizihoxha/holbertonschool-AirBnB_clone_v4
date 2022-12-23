ede75c69494ca164953fd71ffe013e13b259227a
FatjonMileti <tonimileti@yahoo.com>
2022-12-23 02:10:45 -0800

    ...

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -34,11 +34,12 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-
+    cache_id = uuid.uuid4()
     return render_template('3-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
+                           places=places,
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    const amens = {};
+    $('input:checkbox').click(function () {
+      $(this).each(function () {
+        if (this.checked) {
+          amens[$(this).data('id')] = $(this).data('name');
+        } else {
+          delete amens[$(this).data('id')];
+        }
+      });
+      if (Object.values(amens).length > 0) {
+        $('.amenities h4').text(Object.values(amens).join(', '));
+      } else {
+        $('.amenities h4').html('&nbsp');
+      }
+    });
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+      console.log(data);
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      type: 'POST',
+      data: '{}',
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          let place = data[i];
+          $('section.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms + '</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' + place.description + '</div></article>');
+        }
+      }
+    });
+  });
